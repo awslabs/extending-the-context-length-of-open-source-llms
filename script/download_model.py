8096a42607d532422822c9aeb3c7c2a5eebbdc6b
Chen Wu <wuc@amazon.com>
2023-08-01 17:31:59 +0800

    added executable

--- script/download_model.py
+++ script/download_model.py
@@ -1,5 +1,5 @@
 from huggingface_hub import snapshot_download
 
-snapshot_download(repo_id="amazon/falconlite", 
+snapshot_download(repo_id="amazon/FalconLite", 
                   local_dir="/home/ubuntu/model",
                   local_dir_use_symlinks=False)
