c14db5df4a818a8b2c60a47df8c222edd6844111
Chen Wu <wuc@amazon.com>
2023-10-04 08:47:39 +0000

    added SM scripts

---
+++ falconlite2/sm_build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+REPO_NAME=${1:-custom-tgi-ecr}
+ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+REGION=$(aws configure get region)
+
+echo "REPO_NAME: ${REPO_NAME}"
+echo "REGION: ${REGION}"
+echo "ACCOUNT_ID: ${ACCOUNT_ID}"
+
+docker pull ghcr.io/huggingface/text-generation-inference:1.0.3
+
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
+
+aws ecr describe-repositories --repository-names ${REPO_NAME} --region $REGION
+if [ $? -ne 0 ]
+then
+    echo "Creating ECR repository: ${REPO_NAME}"
+    aws ecr create-repository --repository-name $REPO_NAME --region $REGION
+fi
+
+docker build -t $REPO_NAME .
+
+docker tag $REPO_NAME:latest $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO_NAME:latest
+
+docker push $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO_NAME:latest
+
+echo "Container URI:"
+echo "$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO_NAME:latest"
